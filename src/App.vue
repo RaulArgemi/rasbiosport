021fb3461d95d76ed3fae7023ee5eb486497219b
Fabio <fabio.cebral.7e5@itb.cat>
2023-10-25 16:28:11 +0200

    Base de datos

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <router-view></router-view>>
+  <mierdonVue></mierdonVue>
+  <router-view></router-view>
 </template>
 
+
 <script>
+import mierdonVue from './components/mierdonVue.vue';
 export default {
   name: 'App',
-
-};
+  components :  {
+    mierdonVue
+  }
+}
 </script>
 
 <style scoped></style>
---
+++ src/components/mierdonVue.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Datos desde el servidor</h1>
+    <ul>
+      <li v-for="user in datos" :key="user._id">{{ user.id_user }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mierdonVue',
+  data() {
+    return {
+      datos: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => {
+        this.datos = data;
+      })
+      .catch((error) => {
+        console.error('Error al cargar los datos:', error);
+      });
+  },
+};
+</script>
