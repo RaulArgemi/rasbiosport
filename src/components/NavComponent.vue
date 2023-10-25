6153e3788ef6a402bd613e078aafb49fbfb78378
Asier <asier.barranco.7e6@itb.cat>
2023-10-25 15:34:33 +0200

    Modificar css con bootstrap del nav

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,23 +1,21 @@
 <template>
-    <div>
-      <h1>Página de Inicio</h1>
-      <router-link to="/login">
-        <button class="btn btn-primary">Ir a Login</button>
-      </router-link>
-      <router-link to="/register">
-        <button class="btn btn-success">Ir a Registro</button>
-      </router-link>
-      <router-link to="/">
-        <button class="btn btn-success">HOME</button>
-      </router-link>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Página de Inicio</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">Iniciar sesión</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">Registrarse</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">HOME</router-link>
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'NavComponent',
-  };
-  </script>
-  
-  <style scoped></style>
-  
+  </nav>
+</template>
