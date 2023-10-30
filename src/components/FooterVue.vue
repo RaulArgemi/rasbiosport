9a3f09785295b3ee895c2ba663fc60706a046a3c
raul <raul.argemi.7e6@itb.cat>
2023-10-30 16:07:12 +0100

    Footer

---
+++ src/components/FooterVue.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer class="bg-dark text-light py-3">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4">
+            <p class="mb-0 text-center">RASBIOSPORT®</p>
+          </div>
+          <div class="col-md-4 text-center">
+            <p class="mb-0">Política de Privacidad</p>
+          </div>
+          <div class="col-md-4 d-flex justify-content-end align-items-center">
+            <img src="../assets/logo.png" class="text-light me-3">
+            <img src="../assets/logo.png" class="text-light me-3">
+            <img src="../assets/logo.png" class="text-light">
+          </div>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
+<style>
+
+img{
+    width: 50px;
+    height: 50px;
+}
+
+</style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
     <NavComponent></NavComponent>
     <h1>HOLA</h1>
+    <FooterVue></FooterVue>
 </template>
 <style scoped>
 
 </style>
 <script>
 import NavComponent from '../components/NavComponent.vue'
+import FooterVue from '@/components/FooterVue.vue';
  export default {
     name: 'HomeView',
     components: {
-        NavComponent
+        NavComponent,
+        FooterVue
     }
   };
 </script>
