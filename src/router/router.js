bb84325315b52da4ddaf843578bdbabf73a78012
raul <raul.argemi.7e6@itb.cat>
2023-11-20 15:50:16 +0100

    Auth

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,7 @@
+import jwt_decode from 'jwt-decode';
 import { createRouter, createWebHistory } from 'vue-router';
-import Register from '../views/RegisterView.vue'; 
+
+import Register from '../views/RegisterView.vue';
 import Login from '../views/LoginView.vue';
 import Inicio from '../views/HomeView.vue';
 import Privacidad from '../views/PrivacyPolicyView.vue';
@@ -9,8 +11,8 @@ import Ayuda from '../views/HelpView.vue';
 const routes = [
   { path: '/privacy', component: Privacidad },
   { path: '/', component: Inicio },
-  { path: '/register', component: Register }, 
-  { path: '/login', component: Login }, 
+  { path: '/register', component: Register },
+  { path: '/login', component: Login },
   { path: '/profile', component: Perfil, meta: { requiresAuth: true } },
   { path: '/help', component: Ayuda }
 ];
@@ -21,23 +23,30 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-
   const isAuthenticated = checkAuthentication();
 
-  if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/login'); 
+  const token = localStorage.getItem('token');
+
+  if (to.meta.requiresAuth && !isAuthenticated && !isTokenExpired(token)) {
+    next('/login');
   } else {
     next();
   }
 });
 
-
 function checkAuthentication() {
-
   const token = localStorage.getItem('token');
-
-  return token !== null && token !== undefined
+  return token !== null && token !== undefined;
 }
 
+function isTokenExpired(token) {
+  try {
+    const decodedToken = jwt_decode(token);
+    return decodedToken.exp < Date.now() / 1000;
+  } catch (error) {
+    console.error('Error decoding token:', error);
+    return true;
+  }
+}
 
 export default router;
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,42 +1,51 @@
 <template>
-    <NavComponent></NavComponent>
     <div>
+      <NavComponent></NavComponent>
+      <div>
         <ul>
-            <li v-for="user in users" :key="user.id_user">
-                <div>ID: {{ user.id_user }}</div>
-                <div>Nombre: {{ user.name_user }}</div>
-                <div>Correo: {{ user.user_email }}</div>
-                <div>Dirección: {{ user.user_address }}</div>
-            </li>
+          <li v-for="user in users" :key="user.id_user">
+            <div>ID: {{ user.id_user }}</div>
+            <div>Nombre: {{ user.name_user }}</div>
+            <div>Correo: {{ user.user_email }}</div>
+            <div>Dirección: {{ user.user_address }}</div>
+          </li>
         </ul>
+      </div>
+      <FooterVue></FooterVue>
     </div>
-    <FooterVue></FooterVue>
-</template>
+  </template>
   
-<script>
-import NavComponent from '../components/NavComponent.vue'
-import FooterVue from '@/components/FooterVue.vue';
-export default {
-    name: 'LoginView',
+  <script>
+  import NavComponent from '../components/NavComponent.vue';
+  import FooterVue from '@/components/FooterVue.vue';
+  
+  export default {
+    name: 'ProfileView',
     components: {
-        NavComponent,
-        FooterVue
+      NavComponent,
+      FooterVue,
     },
     data() {
-        return {
-            users: [],
-        };
+      return {
+        users: [],
+      };
     },
     mounted() {
-        fetch('http://localhost:3000/api/users')
-            .then((response) => response.json())
-            .then((data) => {
-                this.users = data;
-            })
-            .catch((error) => {
-                console.error('Error al cargar los datos de usuarios:', error);
-            });
-    },
-};
-</script>
+    const token = localStorage.getItem('token');
+    const headers = new Headers({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    });
+
+    fetch('http://localhost:3000/api/profile', { headers })
+      .then((response) => response.json())
+      .then((data) => {
+        this.users = data;
+      })
+      .catch((error) => {
+        console.error('Error al cargar los datos de usuarios:', error);
+      });
+  },
+  };
+  </script>
   
