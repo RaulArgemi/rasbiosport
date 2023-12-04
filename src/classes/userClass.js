5d77af0c66605d5f2f72caca3fb2a082926e7988
Asier <asier.barranco.7e6@itb.cat>
2023-12-04 17:05:36 +0100

    CAMBIO TANGIBLE: PODER EDITAR TU USUARIO Y QUE LOS DATOS SE REFLEJEN EN LA BBDD

--- src/classes/userClass.js
+++ src/classes/userClass.js
@@ -1,12 +1,12 @@
 class Usuari {
 
     constructor(id_user, name_user, user_phone, user_email, user_address, user_role) {
-      this.id = id_user;
-      this.name = name_user;
-      this.phone = user_phone;
-      this.email = user_email;
-      this.address = user_address;
-      this.role = user_role;
+      this.id_user = id_user;
+      this.name_user  = name_user;
+      this.user_phone = user_phone;
+      this.user_email = user_email;
+      this.user_address = user_address;
+      this.user_role = user_role;
     }
   }
   
