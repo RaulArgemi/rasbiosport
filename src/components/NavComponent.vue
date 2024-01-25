a2868cf91d5e908b6904f5c228108b5359817f38
Fabio <fabio.cebral.7e5@itb.cat>
2024-01-25 16:38:36 +0100

    Limpieza del código

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,11 +1,16 @@
 <template>
   <nav>
     <router-link to="/"><img class="logo" src="../assets/rasbiosport-logo-white.png"></router-link>
-    <input type="text" placeholder="Buscar..." class="search-input">
+    <SearchBar></SearchBar>
     <div>
-    <router-link v-if="itsLogged" to="/profile" class="icon"><userIcon></userIcon></router-link>
-      <router-link v-if="!itsLogged" to="/login" class="icon"><logInIcon></logInIcon></router-link>
-      <router-link v-if="!itsLogged" to="/register" class="icon"><img class="header-photo" src="../assets/register.png"></router-link>
+      <router-link v-if="itsLogged" to="/profile" class="icon">
+        <userIcon></userIcon>
+      </router-link>
+      <router-link v-if="!itsLogged" to="/login" class="icon">
+        <logInIcon></logInIcon>
+      </router-link>
+      <router-link v-if="!itsLogged" to="/register" class="icon"><img class="header-photo"
+          src="../assets/register.png"></router-link>
       <LogoutVue v-if="itsLogged"></LogoutVue>
     </div>
   </nav>
@@ -15,13 +20,15 @@
 import LogoutVue from './LogoutVue.vue';
 import userIcon from './icons/userIcon.vue';
 import logInIcon from './icons/logInIcon.vue';
+import SearchBar from './SearchBar.vue';
 
 export default {
   name: 'NavComponent',
   components: {
-    LogoutVue, 
+    LogoutVue,
     userIcon,
     logInIcon,
+    SearchBar
   },
   data() {
     return {
@@ -35,10 +42,10 @@ export default {
     checkAuthentication() {
       const token = localStorage.getItem('token');
       if (token !== null && token !== undefined) {
-        this.itsLogged=true
+        this.itsLogged = true
         console.log('Usuario autenticado');
       } else {
-        this.itsLogged=false
+        this.itsLogged = false
         console.log('Usuario no autenticado');
       }
     },
@@ -52,19 +59,22 @@ export default {
   color: aliceblue;
   margin-right: 2.5rem;
 }
-nav{
+
+nav {
   background-color: #4285F4;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-div{
+
+div {
   display: flex;
   justify-content: end;
   align-items: center;
   margin-right: 40px;
 }
-.logo{
+
+.logo {
   margin-left: 50px;
   margin-top: 5px;
   margin-bottom: 5px;
@@ -73,15 +83,16 @@ div{
   display: flex;
   align-items: center;
 }
-.search-input{
+
+.search-input {
   width: 900px;
   height: 50px;
   border-radius: 15px;
 }
-.header-photo{
+
+.header-photo {
   height: 2.5em;
-  width: 2.5em  ;
+  width: 2.5em;
   margin-left: 10px;
   margin-right: 10px;
-}
-</style>
+}</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="search-bar">
+        <input type="text" v-model="searchTerm" @input="searchProducts" placeholder="Buscar productos..."
+            class="search-input" >
+        <ul v-if="showResults && searchResults.length" class="search-results">
+            <li v-for="product in searchResults" :key="product.product_id" @click="selectProduct(product)">
+                {{ product.product_name }}
+            </li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+import debounce from "lodash/debounce";
+
+export default {
+    name: "SearchBar",
+    data() {
+        return {
+            searchTerm: "",
+            searchResults: [],
+            showResults: false
+        };
+    },
+    methods: {
+        searchProducts: debounce(async function () {
+            this.showResults = true;
+            if (!this.searchTerm) {
+                this.searchResults = [];
+                console.log("Busqueda vacía")
+                return;
+            }
+            try {
+                console.log(this.searchTerm)
+                const response = await fetch(`http://localhost:3000/api/products/search?query=${this.searchTerm}`);
+                const data = await response.json();
+                console.log(data)
+                this.searchResults = data;
+            } catch (error) {
+                console.error('Error al buscar productos:', error);
+            }
+        }, 500),
+        selectProduct(product) {
+            this.searchTerm = '';
+            this.showResults = false;
+            this.$router.push({ name: 'ProductDetails', params: { product_name: product.product_name } });
+        },
+    }
+};
+</script>
