ad21daa96e56f08767170867b89addb95968c450
Fabio <fabio.cebral.7e5@itb.cat>
2024-02-22 20:02:16 +0100

    Agregar desde el detalle

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -10,12 +10,20 @@
       <div class="product-info">
         <h2>{{ productDetails.product_name }}</h2>
         <p class="product-description">{{ productDetails.product_desc }}</p>
-        <p><strong>Tallas disponibles:</strong> {{ productDetails.product_size }}</p>
         <p><strong>Precio:</strong> {{ productDetails.product_price }} €</p>
-        <!-- Agregar más detalles del producto si es necesario -->
         <p><strong>Información adicional:</strong> {{ productDetails.product_info }}</p>
         <p><strong>Etiqueta:</strong> {{ productDetails.product_tag }}</p>
-        <button class="buy" @click="addToCart(product_id)">Añadir al carrito</button>
+        <button v-if="!optionsVisible" @click="showOptions('abrir')" class="add-to-cart-btn">Añadir al carrito</button>
+    <button v-if="optionsVisible" @click="showOptions('cerrar')" class="close-options-btn">Cerrar</button>
+    <div v-if="optionsVisible" class="options-container">
+      <label for="size" class="options-label">Talla:</label>
+      <select v-model="selectedSize" id="size" @change="updateQuantityLimit" class="options-select">
+        <option v-for="size in sizes" :key="size.size" :value="size.size">{{ size.size }}</option>
+      </select>
+      <label for="quantity" class="options-label">Cantidad:</label>
+      <input v-model.number="selectedQuantity" type="number" id="quantity" :max="quantityLimit" min="1" class="options-input">
+      <button @click="addToCart(product_id)" :disabled="!selectedSize" class="add-to-cart-btn">Agregar al carrito</button>
+    </div> 
       </div>
     </div>
 
@@ -72,48 +80,80 @@ export default {
       productDetails: {},
       productReviews: [],
       relatedProducts: [],
+      optionsVisible: false,
       selectedSize: '',
+      selectedQuantity: 1,
+      sizes: [],
+      quantityLimit: 0,
     };
   },
   async created() {
     await this.initializeData();
+    console.log(JSON.stringify(this.productDetails))
   },
   methods: {
-    async addToCart(productId) {
-    try {
-      const userId = JSON.parse(Cookies.get('userData')).id_user;
-      console.log(userId);
-      console.log(productId);
 
-      await this.getSizes(productId);
+    updateQuantityLimit() {
+      const selectedSizeObject = this.sizes.find(item => item.size === this.selectedSize);
+      if (selectedSizeObject) {
+        this.quantityLimit = selectedSizeObject.stock;
+        if (this.selectedQuantity > this.quantityLimit) {
+          this.selectedQuantity = this.quantityLimit;
+        }
+      }
+    },
 
-      fetch(`${url}/api/cart/add`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ userId, productId }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data.message);
+    showOptions(order) {
+      if (order == 'abrir') {
+        this.optionsVisible = !this.optionsVisible;
+        this.getSizes(this.product_id)
+      } else {
+        this.optionsVisible = !this.optionsVisible;
+
+      }
+    },
+
+    async getSizes() {
+      try {
+        const response = await fetch(`${url}/api/get/tallas/${this.productDetails.product_id}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const data = await response.json();
+        console.log('Respuesta recibida:', data);
+        this.sizes = data.result.map(item => ({
+          size: item.product_size,
+          stock: item.product_stock,
+        }));
+      } catch (error) {
+        console.error('Error al obtener tallas:', error);
+      }
+    },
+
+
+    async addToCart() {
+      try {
+        const userId = JSON.parse(Cookies.get('userData')).id_user;
+        console.log(userId);
+        console.log(this.productDetails.product_id);
+        console.log(this.selectedSize)
+
+        fetch(`${url}/api/cart/add`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ userId, productId: this.productDetails.product_id, size: this.selectedSize, quantity: this.selectedQuantity}),
         })
-        .catch(error => console.error('Error al añadir al carrito:', error));
-    } catch (error) {
-      console.error('Error al ejecutar addToCart:', error);
-    }
-  },
-  async getSizes(productId) {
-    try {
-      const response = await fetch(`${url}/api/get/tallas/${productId}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      });
+          .then(response => response.json())
+          .then(data => {
+            console.log(data.message);
+          })
+          .catch(error => console.error('Error al añadir al carrito:', error));
+      } catch (error) {
+        console.error('Error al ejecutar addToCart:', error);
+      }
+    },
 
-      const data = await response.json();
-      console.log('Respuesta recibida:', data);
-    } catch (error) {
-      console.error('Error al obtener tallas:', error);
-    }
-  },
     formatDate(dateString) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' };
       return new Date(dateString).toLocaleDateString(undefined, options);
@@ -151,7 +191,7 @@ export default {
     goToProductDetails(productName) {
       this.$router.push({ name: 'ProductDetails', params: { product_name: productName } });
     },
-  },
+
   computed: {
     filteredRelatedProducts() {
       return this.relatedProducts.filter(product => product.product_id !== this.productDetails.product_id);
@@ -161,8 +201,7 @@ export default {
     $route() {
       this.initializeData();
     },
-  },
-};
+  },}};
 </script>
 
 <style scoped>
@@ -296,5 +335,62 @@ export default {
 .review-stars .text-warning {
   color: #FFD700;
   font-size: 1.2em;
+}
+.add-to-cart-btn,
+.close-options-btn {
+  background-color: #007bff;
+  color: #fff;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 12px;
+}
+
+.close-options-btn {
+  background-color: #dc3545;
+}
+
+.options-container {
+  margin-top: 16px;
+}
+
+.options-label {
+  display: block;
+  margin-bottom: 4px;
+  font-size: 14px;
+}
+
+.options-select,
+.options-input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.add-to-cart-btn:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+
+.add-to-cart-btn:hover {
+  background-color: #0056b3;
+}
+.buy {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.buy:hover {
+  background-color: #0056b3;
 }
 </style>
