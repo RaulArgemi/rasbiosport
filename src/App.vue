e0407ca42ecc9768191cf1a2d8a805279cd4c6cc
raul <raul.argemi.7e6@itb.cat>
2024-02-22 18:43:32 +0100

    css

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,30 @@ import "./assets/main.css"
 
 export default {
   name: 'App',
-  components: { FooterVue }
+  components: { FooterVue },
+  created() {
+    this.checkSessionAndFetchUserData();
+  },
+  methods: {
+    async checkSessionAndFetchUserData() {
+      const token = localStorage.getItem('token');
+      if (token) {
+        try {
+          const response = await fetch(`${url}/api/me`, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          });
+          const userData = await response.json();
+          if (response.ok) {
+            this.$store.dispatch('setUser', userData);
+          } 
+        } catch (error) {
+          console.error('Error al obtener los datos del usuario:', error);
+        }
+      }
+    }
+  }
 };
 </script>
 
